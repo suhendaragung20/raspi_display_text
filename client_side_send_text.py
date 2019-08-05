3a95d05046a3cea3b535a4faa1906e0cd7f8a4ad
A_S <suhendaragung20@gmail.com>
2019-08-05 14:29:36 +0700

    aa

--- client_side_send_text.py
+++ client_side_send_text.py
@@ -1,20 +1,7 @@
 import socket
 import threading
 import sys
-import cv2
 
-
-#Wait for incoming data from server
-#.decode is used to turn the message in bytes to a string
-def receive(socket, signal):
-    while signal:
-        try:
-            data = socket.recv(32)
-            print(str(data.decode("utf-8")))
-        except:
-            print("You have been disconnected from the server")
-            signal = False
-            break
 
 #Get host and port
 host = "192.168.43.150"
@@ -28,10 +15,6 @@ except:
     print("Could not make a connection to the server")
     input("Press enter to quit")
     sys.exit(0)
-
-#Create new thread to wait for data
-receiveThread = threading.Thread(target = receive, args = (sock, True))
-receiveThread.start()
 
 #Send data to server
 #str.encode is used to turn the string message into bytes so it can be sent across the network
--- server_side_display_text.py
+++ server_side_display_text.py
@@ -4,47 +4,43 @@ import cv2
 
 
 
-def main():
-    # Get host and port
-    host = "192.168.43.150"
-    port = 60000
+# Get host and port
+host = "192.168.43.150"
+port = 60000
 
-    # Create new server socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((host, port))
-    sock.listen(5)
+# Create new server socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(5)
 
-    sock_client, address_client = sock.accept()
+sock_client, address_client = sock.accept()
 
-    print("Connected with " + str(address_client))
+print("Connected with " + str(address_client))
 
-    text_display = ""
+text_display = ""
 
-    while True:
-        frame = cv2.imread("screen.png")
+while True:
+    frame = cv2.imread("screen.png")
 
-        key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1) & 0xFF
 
-        try:
-            data = sock_client.recv(32)
-        except:
-            print("Client " + str(address_client) + " has disconnected")
-            break
-        if data != "":
-            print(str(data.decode("utf-8")))
-            text_display = str(data.decode("utf-8"))
+    try:
+        data = sock_client.recv(32)
+    except:
+        print("Client " + str(address_client) + " has disconnected")
+        break
+    if data != "":
+        print(str(data.decode("utf-8")))
+        text_display = str(data.decode("utf-8"))
 
-        #cv2.putText(frame, text_display, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 2)
+    #cv2.putText(frame, text_display, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 2)
 
-        cv2.namedWindow("Frame", cv2.WINDOW_KEEPRATIO)
-        cv2.setWindowProperty("Frame", cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
-        cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.namedWindow("Frame", cv2.WINDOW_KEEPRATIO)
+    cv2.setWindowProperty("Frame", cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+    cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-        cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", frame)
 
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
-
-
-main()
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
